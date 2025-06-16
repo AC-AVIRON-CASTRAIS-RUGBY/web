29f41dcbc67f4bf8d4eb01606ede71690e1bbfae
Alexis RARCHAERT <bonjour@alexis-rarchaert.fr>
2025-06-16 16:38:59 +0200

    Add favicon files: PNG and SVG versions
    - Added a new PNG favicon file with a size of 32x32 pixels.
    - Created a new SVG favicon file featuring a circular design with a blue background and a white letter "A" in the center.

--- src/views/includes/header.php
+++ src/views/includes/header.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
+<head>    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Application Aviron Castrais</title>    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <title>Application Aviron Castrais</title>
+    <link rel="icon" href="img/favicon/favicon.ico">
+    <link rel="icon" type="image/svg+xml" href="img/favicon/favicon.svg">
+    <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/teams.css">
